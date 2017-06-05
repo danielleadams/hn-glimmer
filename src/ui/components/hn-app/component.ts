54edffc5a801f496990d6cbfb11903c1c5993955
Danielle Adams <adamzdanielle@gmail.com>
2017-06-04 23:10:06 -0400

    add hn, web stories, and details components

--- src/ui/components/hn-app/component.ts
+++ src/ui/components/hn-app/component.ts
@@ -1,5 +1,17 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from '@glimmer/component';
 
 export default class Hn extends Component {
+  @tracked results: any;
 
+  constructor(options) {
+    super(options);
+    this.loadResults();
+  }
+
+  async loadResults() {
+    let request = await fetch('http://node-hnapi.herokuapp.com/news');
+    let results = await request.json();
+
+    this.results = results;
+  }
 }
